839750be7920203409605e4f4d85a1f9219a4117
Artemi <artemi@tktk.ee>
2023-12-07 17:03:42 +0200

    Epic

---
+++ Week8EpicGame/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+string folderPath = @"C:\data";
+string heroFile = "heroes.txt";
+string villainFile = "villains.txt";
+
+string[] heroes = File.ReadAllLines(Path.Combine(folderPath, heroFile));
+
+string[] villains = File.ReadAllLines(Path.Combine(folderPath, villainFile));
+
+//string[] heroes = {"Harry Potter", "Luke Skywalker", "Lara Croft", "Sherlock Holmes"};
+//string[] villains = { "Joker", "Voldemort", "Darth Vader", "Sauron", "Dracula" };
+
+
+string[] weapon = { "magic wand", "banana", "plastic fork", "sword", "brick" };
+
+string hero = GetRandomValueFromArray(heroes);
+string heroWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($" Today {hero} with {heroWeapon} saves the day!");
+
+
+string villain = GetRandomValueFromArray(villains);
+string villainWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {villain} with {villainWeapon} tries to take over the world.");
+
+
+static string GetRandomValueFromArray(string[] someArray)
+{
+    Random rnd = new Random();
+    int randomIndex = rnd.Next(0, someArray.Length);
+    string randomStringFromArray = someArray[randomIndex];
+    return randomStringFromArray;
+}
+
